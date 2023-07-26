a20039778126170c3124b5101f021ecfdea39aa0
muhammad-shahin <muhammadshahin002@gmail.com>
2023-07-26 12:55:29 +0600

    slider added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    colors: {
-      'primaryBtn': "#B68C5A",
-      'secondary': '#414040',
-      'des': '#707070',
-    },
     extend: {},
   },
   plugins: [],
